f045399b9f21cffff322e38afbf44da91cb7a590
trtiger4520 <trtiger4520@gmail.com>
2024-06-29 20:47:08 +0800

    fix url base

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ import './styles.css';
 
 import { routes } from './routers';
 
-export const createApp = ViteSSG(App, { routes });
+export const createApp = ViteSSG(App, { routes, base: import.meta.env.BASE_URL });
